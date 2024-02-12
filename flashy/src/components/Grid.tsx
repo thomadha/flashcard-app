58dcce9b9599f5e708913abf2ae4f6e2654d690d
Caroline Gaard <carolgaa@stud.ntnu.no>
2024-02-12 15:00:37 +0100

    Added HomePage components

---
+++ flashy/src/components/Grid.tsx
@@ -0,0 +1,48 @@
+function Grid() {
+    return (
+        <>
+            <div className="GridBody"></div>
+            <div className="GridBody">
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    b
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+                <div className="SetBox">
+                    a
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Grid
