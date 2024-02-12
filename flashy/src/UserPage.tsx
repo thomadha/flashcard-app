4556810d165399768c52c1c9865ae691d445f60e
David Salguero Spilde <davidssp@stud.ntnu.no>
2024-02-12 12:55:46 +0100

    Rebase with dev after task completion

---
+++ flashy/src/UserPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+
