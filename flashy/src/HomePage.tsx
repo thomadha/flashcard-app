be55263c40a0cd785cf234ced24eb4d9e2c7c0f8
Caroline Gaard <carolgaa@stud.ntnu.no>
2024-02-12 15:00:37 +0100

    Added HomePage components

---
+++ flashy/src/HomePage.tsx
@@ -0,0 +1,12 @@
+import "./CardViewer.css";
+import HomePageParent from "./components/HomePageParent";
+
+function HomePage() {
+  return (
+    <div>
+      <HomePageParent />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ flashy/src/components/HomePageNav.tsx
@@ -0,0 +1,11 @@
+function HomePageNav() {
+    return (
+        <div style={{ backgroundColor: "#DEFEDD" }} className="Container">
+            <button id="NoBorderButton">Mine sett</button>
+            <button id="NoBorderButton">Utforsk</button>
+            <button id="CreateSetButton">Lag et nytt sett</button>
+        </div>
+    )
+}
+
+export default HomePageNav
