deb53e2bbe24db8225d60c202c714fe6adcee8aa
David Salguero Spilde <davidssp@stud.ntnu.no>
2024-02-14 16:30:18 +0100

    Added script to create itemdivs (not complete)

--- flashy/src/components/Grid.tsx
+++ flashy/src/components/Grid.tsx
@@ -1,44 +1,31 @@
+import * as React from "react";
+
+export interface Item {
+    //Kan starte med bare navn på settet også utvide etterhvert
+    SetName: String;
+}
+
+export interface GridItemArray {
+    items: {
+        item: Item;
+    };
+}
+
 function Grid() {
+
+    const currentArray: GridItemArray = {
+        //For loop som henter fra Firebase
+    }
+
+    //lager alle divsa fra verdiene i arrayen 
+    const Itemdivs: JSX.Element[] = [];
+    for (const key in )
+
     return (
         <>
             <div className="GridBody"></div>
             <div className="GridBody">
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    b
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
-                <div className="SetBox">
-                    a
-                </div>
+                {Itemdivs}
             </div>
         </>
     )
