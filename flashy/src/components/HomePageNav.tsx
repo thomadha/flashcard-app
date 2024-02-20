d4171e5a1ebd7a398059fd601a79560b4b54fc12
Markus Hansen Selnes <markuhse@stud.ntnu.no>
2024-02-20 15:31:26 +0100

    redirect to CardEditor when clicking "legg til nytt sett"

--- flashy/src/components/HomePageNav.tsx
+++ flashy/src/components/HomePageNav.tsx
@@ -1,11 +1,20 @@
+import { Link, useNavigate } from "react-router-dom";
+
 function HomePageNav() {
+
+    const navigateTo = useNavigate();
+
+    const goToEdit = () => {
+        navigateTo("/edit");
+      };
+
     return (
         <div style={{ backgroundColor: "#DEFEDD" }} className="Container">
             <button id="HomePageNavButton">Mine sett</button>
             <button id="HomePageNavButton">Utforsk</button>
             <button id="HomePageNavButton">Favoritter</button>
             <button id="SearchSetButton">Søk</button>
-            <button id="CreateSetButton">Lag et nytt sett</button>
+            <button id="CreateSetButton" onClick={goToEdit}>Lag et nytt sett</button>
         </div>
     )
 }
