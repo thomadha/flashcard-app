ed2ce07f09b8bc10ab7dc902e71f4c03db4ca49f
Emil Garcia-Godos Wiig <egwiig@stud.ntnu.no>
2024-02-20 12:10:55 +0100

    Changed useCardstrings function, thus changing architecture of cardsdata and studyset, so that updates in set are continuous.

--- flashy/src/components/FlashCardEditor.tsx
+++ flashy/src/components/FlashCardEditor.tsx
@@ -59,8 +59,7 @@ const Page: React.FC = () => {
         }
       }, [cardsData]);
 
-    // Updates cardsData with function from useCardStrings, when dummy value is changed
-    // Dummy value is changed when updating, adding or deleting card.
+    // Constant updating of setStudySet
     useEffect(() => { 
         fetchData();
         
