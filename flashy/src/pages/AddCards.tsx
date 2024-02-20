79dd7beb8647049be04ff416a50c9dac4ee57601
Markus Hansen Selnes <markuhse@stud.ntnu.no>
2024-02-20 14:59:10 +0100

    move AddCards into pages

--- flashy/src/pages/AddCards.tsx
+++ flashy/src/pages/AddCards.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import FlashCardEditor from "./components/FlashCardEditor";
+import NavBar from "../components/NavBar";
+import FlashCardEditor from "../components/FlashCardEditor";
 import "./CardViewer.css";
 
 function AddCards() {
