d8b41403959453ea247b4a6feaa9d7a2260a123c
Thomas Haugli <thomas.djonne.haugli@gmail.com>
2024-02-21 13:53:55 +0100

    Now possible to see list of admin users

--- flashy/src/components/AdminPanel.tsx
+++ flashy/src/components/AdminPanel.tsx
@@ -2,29 +2,35 @@ import { useEffect, useState } from "react";
 import { doc, updateDoc, arrayUnion, arrayRemove, getDoc } from "firebase/firestore";
 import { db } from "../lib/firebase/firebase";
 import { getAuth } from "firebase/auth";
+import { Link } from "react-router-dom";
 
 function AdminPanel(){
     const [email, setEmail] = useState("");
     const [isAdmin, setIsAdmin] = useState(false);
     const adminRef = doc(db, "Administratorer", "UsersWithAdmin");
+    const [AdminArray, setAdminArray] = useState<string[]>([]);
+    
 
     useEffect(() => {
         const checkAdminStatus = async () => {
-            const isAdminResult = await CheckIfAdmin();
-            setIsAdmin(isAdminResult);
+            const isAdminResult= await CheckIfAdmin();
+            if(isAdminResult){
+                setIsAdmin(true); 
+            }
         };
         checkAdminStatus();
     }, []);
 
     async function CheckIfAdmin(){
+        
         const auth = getAuth();
         const user = auth.currentUser;
-        if(user != null){
+        if(user != null && user.email != null){
             const mail = user.email;
             const arrayRef = doc(db, "Administratorer", "UsersWithAdmin");
             const AdminArrayDoc = await getDoc(arrayRef); 
-            const AdminArray = AdminArrayDoc.get("AdminArray"); 
-            return AdminArray.includes(mail);
+            setAdminArray(AdminArrayDoc.get("AdminArray")); 
+            return AdminArrayDoc.get("AdminArray").includes(mail);
         } else{
             return false; 
         }
@@ -33,33 +39,41 @@ function AdminPanel(){
     async function addUser(event: React.SyntheticEvent) {
         event.preventDefault();
         await updateDoc(adminRef, {AdminArray: arrayUnion(email)})
-        console.log(CheckIfAdmin); 
+        if(!AdminArray.includes(email)){
+            setAdminArray([...AdminArray, email]);
+        }
     }
 
     async function removeUser(event: React.SyntheticEvent) {
         event.preventDefault();
         await updateDoc(adminRef,{AdminArray: arrayRemove(email)})
+        setAdminArray(AdminArray.filter(item => item !== email));
     }
 
     return(
         <div id="divUser">
             {isAdmin && (
-                <form id="UsersWithAccess" >
-                <p id="text">Skriv inn bruker du ønsker å gi/fjerne adminrettigheter</p>
-                <input
-                    id="emailInput"
-                    placeholder="E-post"
-                    type="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <br/><br/>
-                <button id="addAdminBtn" onClick={addUser}>
-                    Legg til
-                </button>
-                <button id="removeAdminBtn" onClick={removeUser}>
-                    Fjern
-                </button>
-                </form>
+                <div>
+                    {AdminArray.map((item, index)=>(
+                        <li key={index}>{item}</li>
+                    ))}
+                    <form id="UsersWithAccess" >
+                        <p id="text">Skriv inn bruker du ønsker å gi/fjerne adminrettigheter</p>
+                        <input
+                            id="emailInput"
+                            placeholder="E-post"
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <br/><br/>
+                        <button id="addAdminBtn" onClick={addUser}>
+                            Legg til
+                        </button>
+                        <button id="removeAdminBtn" onClick={removeUser}>
+                            Fjern
+                        </button>
+                    </form>
+                </div>
             )}
             {!isAdmin && (
                 <p id="text">Du har dessverre ikke adminrettigheter</p>
