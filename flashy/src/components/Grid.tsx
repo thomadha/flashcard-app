3b9fe3d04847478a8ef7ba0ff3c097d81dfefa89
Markus Hansen Selnes <markuhse@stud.ntnu.no>
2024-02-21 14:34:30 +0100

    allow users to delete sets

--- flashy/src/components/Grid.tsx
+++ flashy/src/components/Grid.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import useSetNames from "./FetchSetNames";
 import { useNavigate } from "react-router-dom";
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../lib/firebase/firebase";
 
 export interface Item {
     id: string; // Add id property
@@ -32,6 +34,20 @@ function Grid(){
         const editArray = [id, "", false];
         navigateTo("/edit", { state: { editArray } });
     }
+
+    const deleteSet = (id: string) => async (event: React.MouseEvent) => {
+        event.stopPropagation();
+        try {
+            if (id !== undefined) {
+                console.log("Deleting ", id);
+                const docRef = doc(db, "flashcardSets", id);
+                await deleteDoc(docRef);
+            }
+        }
+        catch (e) {
+            return;
+        }
+    }
   
     return (
         <>
@@ -40,6 +56,7 @@ function Grid(){
                     <div key={item.id} className="grid-item" onClick={() => gotoPage(item.id)}>
                         <div>{item.name}</div>
                         <button onClick={gotoEdit(item.id)}>Rediger</button>
+                        <button className="deleteButton" onClick={(event) => deleteSet(item.id)(event)}> Slett </button>
                     </div>
                 ))}
             </div>
