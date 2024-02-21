5bc4265c967707b864da3711ded6f0b96602ad56
David Salguero Spilde <davidssp@stud.ntnu.no>
2024-02-21 11:03:04 +0100

    Revert "IKKE MERGE DENNE INN I DEV"
    This reverts commit f55c6be356edac82beb26d1c065e15c35dd513fc.

--- flashy/src/components/FlashCardSet.tsx
+++ flashy/src/components/FlashCardSet.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../lib/firebase/firebase";
+import exp from 'constants';
 
-export const FetchFlashCardset = (flashCardSetId: string) => {
-  const [cardsData, setCardsData] = useState<string[][] | null>(null);
-  const [docId, setnData] = useState<string>(flashCardSetId);
+export const useCardStrings = (flashCardSetId: string) => {
+    const [cardsData, setCardsData] = useState<string[][] | null>(null);
   
 
     const fetchData = async () => {
-      const cardsCollectionRef = collection(db, 'flashcardSets', docId);
+      const cardsCollectionRef = collection(db, 'flashcardSets', flashCardSetId, 'cards');
       const querySnapshot = await getDocs(cardsCollectionRef);
       const data = querySnapshot.docs.map(doc => [doc.data().flashcardFront, doc.data().flashcardBack, doc.id]);
       setCardsData(data);
@@ -19,24 +19,7 @@ export const FetchFlashCardset = (flashCardSetId: string) => {
       fetchData();
     }, [flashCardSetId]);
   
-    return {cardsData, fetchData, docId};
+    return {cardsData, fetchData};
 }
 
-export const AddNewFlashcardset = () => {
-    const [cardsData, setCardsData] = useState<string[][]>([["","","",""]]);
-    const [docId, setnData] = useState("");
-
-  const fetchData = async () => {
-    const collectionRef = collection(db, 'flashcardSets');
-    const docRef = await addDoc(collectionRef, {
-      field1: "Hei",
-      field2: "heisann"
-    });
-      let a = docRef.id;
-      setnData(a);
-    };
-  fetchData();
-  return {cardsData, fetchData};
-}
-
-export default AddNewFlashcardset;
+export default useCardStrings;
