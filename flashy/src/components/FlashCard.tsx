6e7df4e36a4aec4378ef1bd7a62d5d8cc7ead4cf
Helle Elvebakk <hellene@stud.ntnu.no>
2024-02-22 12:18:49 +0100

    Difficultstate updated

--- flashy/src/components/FlashCard.tsx
+++ flashy/src/components/FlashCard.tsx
@@ -1,15 +1,32 @@
+import React, { useState } from "react";
+
 interface FlashCardProps {
   text: string;
   handleClickOnFlashCard: () => void;
+  handleDiffficultState: () => void;
 }
 
-function FlashCard({ text, handleClickOnFlashCard }: FlashCardProps) {
+function FlashCard({
+  text,
+  handleClickOnFlashCard,
+  handleDiffficultState,
+}: FlashCardProps) {
+  const [isDuplicatedVisible, setIsDuplicatedVisible] = useState(false);
+  const handleDifficultClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    handleDiffficultState();
+    setIsDuplicatedVisible(!isDuplicatedVisible);
+  };
+
   return (
     <>
       <div id="flashCard" onClick={handleClickOnFlashCard}>
         <p id="flashCardText">{text}</p>
-        <button id="DifficultCard">Vanskelig</button>
+        <button id="DifficultCard" onClick={handleDifficultClick}>
+          Vanskelig
+        </button>
         <button id="EditFlashCard">Rediger</button>
+        {isDuplicatedVisible && <p id="DuplicatedCard">Prøv deg på denne igjen!</p>}
       </div>
     </>
   );
--- flashy/src/components/FlashCardParent.tsx
+++ flashy/src/components/FlashCardParent.tsx
@@ -3,31 +3,59 @@ import Buttons from "./Buttons";
 import FlashCard from "./FlashCard";
 import UseCardStrings from "./FlashCardSet";
 import { useLocation } from "react-router-dom";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../lib/firebase/firebase";
 
 interface FlashCardParentProps {}
 
 function FlashCardParent(props: FlashCardParentProps) {
-
-    const location = useLocation();
-    const id = location.state.id;
+  const location = useLocation();
+  const id = location.state.id;
   // const { cardsData, fetchData} = UseCardStrings("uL5B3RmmHwv8fI57sdPy");
   //Cardsdata har [0] framsiden, [1] baksiden, [2] IDen til card collection
-    const { cardsData, fetchData } = UseCardStrings();
-    //Framside og bakside av Flashcards
-    const [studySet, setStudySet] = useState([[ "Laster inn..", "Laster inn.."]]);
-    const [card, setCard] = useState(0); 
-    const [side, setSide] = useState(0); 
-    const [text, setText] = useState(studySet[0][0]);
-    useEffect(() => {
-      fetchData(id);
-    }, []);
-  
-    useEffect(() => {
-      if (cardsData) {
-        const extractedData = cardsData.map(card => [card[0], card[1]]);
-        setStudySet(extractedData);
-      }
-    }, [cardsData]);
+  const { cardsData, fetchData } = UseCardStrings();
+  //Framside og bakside av Flashcards
+  const [studySet, setStudySet] = useState([
+    ["Laster inn..", "Laster inn..", "Laster inn...", false],
+  ]);
+  const [card, setCard] = useState(0);
+  const [side, setSide] = useState(0);
+  const [text, setText] = useState(studySet[0][0]);
+  const [isDuplicatedVisible, setIsDuplicatedVisible] = useState(false);
+
+  useEffect(() => {
+    fetchData(id);
+  }, []);
+
+/*   useEffect(() => {
+    if (studySet[card][3] === true) {
+      setIsDuplicatedVisible(Boolean(studySet[card][3]));
+    }
+  }, [card]); */
+
+  useEffect(() => {
+    if (cardsData) {
+      const extractedData = cardsData.map((card) => [
+        card[0],
+        card[1],
+        card[2],
+        Boolean(card[3]),
+      ]);
+      const duplicatedCards = cardsData.reduce((extractedData, currentCard) => {
+        extractedData.push(currentCard);
+        if (currentCard[3]) {
+          extractedData.push([
+            currentCard[0],
+            currentCard[1],
+            currentCard[2],
+            currentCard[3],
+          ]);
+        }
+        return extractedData;
+      }, [] as typeof cardsData);
+      setStudySet(extractedData);
+    }
+  }, [cardsData]);
 
   useEffect(() => {
     if (studySet[card]) {
@@ -81,6 +109,59 @@ function FlashCardParent(props: FlashCardParentProps) {
     setCard(newCardIndex !== -1 ? newCardIndex : 0);
   };
 
+  const handleDifficultState = async () => {
+    if (studySet[card]) {
+      const cardId = studySet[card][2];
+      const currentDifficultState = studySet[card][3];
+      try {
+        await updateDoc(doc(db, "flashcardSets", id, "cards", String(cardId)), {
+          difficultState: !currentDifficultState,
+        });
+        console.log(
+          `Difficult state updated successfully - now ${!currentDifficultState}`
+        );
+
+        setStudySet((studySet) => {
+          const updatedStudySet = [...studySet];
+          updatedStudySet[card][3] = !currentDifficultState;
+
+          // Duplicate the card in studySet if difficultState is set to true
+          if (!currentDifficultState) {
+            updatedStudySet.push([
+              updatedStudySet[card][0],
+              updatedStudySet[card][1],
+              updatedStudySet[card][2] + "-duplicate",
+              updatedStudySet[card][3],
+            ]);
+            console.log("Card duplicated");
+          } else {
+            // If difficultState is set to false, remove the duplicated card
+            const duplicateIndex = updatedStudySet.findIndex(
+              (card) => card[2] === `${cardId}-duplicate`
+            );
+
+            const originalIndex = updatedStudySet.findIndex(
+              (card) => card[2] === String(cardId).split("-", 1)[0]
+            );
+
+            if (duplicateIndex !== -1) {
+              updatedStudySet.splice(duplicateIndex, 1);
+              console.log("Duplicated card removed");
+            }
+
+            if (originalIndex !== duplicateIndex) {
+              updatedStudySet.splice(duplicateIndex, 1);
+              console.log("Duplicated card removed");
+            }
+          }
+          return updatedStudySet;
+        });
+      } catch (error) {
+        console.error("Error updating difficult state:", error);
+      }
+    }
+  };
+
   return (
     <>
       <Buttons
@@ -88,7 +169,11 @@ function FlashCardParent(props: FlashCardParentProps) {
         handleNextClick={handleNextClick}
         handleShuffleCards={handleShuffleCards}
       />
-      <FlashCard text={text} handleClickOnFlashCard={handleClickOnFlashCard} />
+      <FlashCard
+        text={String(text)}
+        handleClickOnFlashCard={handleClickOnFlashCard}
+        handleDiffficultState={handleDifficultState}
+      />
     </>
   );
 }
