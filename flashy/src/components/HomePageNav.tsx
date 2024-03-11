3d8c5a79d9171ae9ce0e517e8f85ab47ac8fc24e
Emil Garcia-Godos Wiig <egwiig@stud.ntnu.no>
2024-03-11 16:09:44 +0100

    Revert "Merge branch 'Task-31' into 'dev'"
    This reverts merge request !23

--- flashy/src/components/HomePageNav.tsx
+++ flashy/src/components/HomePageNav.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { db, auth } from "../lib/firebase/firebase";
-import { useSetTags } from "./FetchFirestoreData";
 
 interface HomePageNavProps {
     filter: string;
@@ -16,10 +15,6 @@ const HomePageNav: React.FC<HomePageNavProps> = ({filter, setFilter, searchItem,
     const navigateTo = useNavigate();
     const [showModal, setShowModal] = useState(false);
     const [setName, setSetName] = useState("");
-
-    const {flashcardTags, fetchSetTags} = useSetTags();
-    const [flashcardSetChosenTag, setFlashcardSetChosenTag] = useState<string>("");
-    const [isOptionsVisible, setOptionsVisible] = useState(false);
 
     const user = auth.currentUser
 
@@ -40,7 +35,7 @@ const HomePageNav: React.FC<HomePageNavProps> = ({filter, setFilter, searchItem,
         setShowModal(false);
         gotoEdit(setName, true);
     }
-    const handlePageChange = (verdi : number) => {
+    const handlemip = (verdi : number) => {
         updatePage(verdi)
     }
 
@@ -55,53 +50,23 @@ const HomePageNav: React.FC<HomePageNavProps> = ({filter, setFilter, searchItem,
         setFilter('')
     }
 
-    const handleButtonClick = () => {
-        // Toggle the visibility of the options div when the button is clicked
-        fetchSetTags("");
-        setOptionsVisible(!isOptionsVisible);
-    };
-
-    const handleTagSelection = (tag:string) => {
-        // Update the selected tag and hide the options div when a tag is selected
-        setFlashcardSetChosenTag(tag);
-        setOptionsVisible(false);
-        setSearchItem(tag)
-    };
- 
     const handleInputChange = (event:React.ChangeEvent<HTMLInputElement>) => { 
         setSearchItem(event.target.value)
     }
 
     return (
         <div style={{ backgroundColor: "#DEFEDD" }} className="Container">
-            <button id="HomePageNavButton" onClick={() => handlePageChange(0)}>Mine sett</button>
-            <button id="HomePageNavButton" onClick={() => handlePageChange(1)}>Utforsk</button>
-            <button id="HomePageNavButton" onClick={() => handlePageChange(2)}>Favoritter</button>
-            <button onClick={handleButtonClick}>Kategorier</button>
-            {isOptionsVisible && (
-            <div className='tagBox' style={{ display: 'flex', flexDirection: 'column' }}>
-                {flashcardTags.map((tag, index) => (
-                    <div key={index} onClick={() => handleTagSelection(tag.tag)} 
-                    style={{
-                        padding: '5px',
-                        cursor: 'pointer',
-                        backgroundColor: flashcardSetChosenTag === tag.tag ? '#EF8CAD' : 'transparent',
-                    }}>
-                        {tag.tag}
-                    </div>
-                ))}
-            </div>
-            )}
+            <button id="HomePageNavButton" onClick={() => handlemip(0)}>Mine sett</button>
+            <button id="HomePageNavButton" onClick={() => handlemip(1)}>Utforsk</button>
+            <button id="HomePageNavButton" onClick={() => handlemip(2)}>Favoritter</button>
             <div className="search-bar"> 
                 <input
                     type="text"
                     value={searchItem}
                     onChange={handleInputChange}
-                    placeholder='Søk...'
+                    placeholder='Type to search'
                 />
             </div>
-        
-
             <button id="CreateSetButton" onClick={handleCreateSet}>Lag et nytt sett</button>
             {showModal && (
                 <div>
@@ -110,7 +75,6 @@ const HomePageNav: React.FC<HomePageNavProps> = ({filter, setFilter, searchItem,
                 </div>
             )}
         </div>
-    
     )
 }
 export default HomePageNav;
